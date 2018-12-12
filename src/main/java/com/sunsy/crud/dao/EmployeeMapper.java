46df9e54aba0464cdd2421dfcdd8085570b80968
sunsy <ssystc@163.com>
2018-12-12 16:19:46 +0800

    ssm application file and mybatis generator

---
+++ src/main/java/com/sunsy/crud/dao/EmployeeMapper.java
@@ -0,0 +1,30 @@
+package com.sunsy.crud.dao;
+
+import com.sunsy.crud.bean.Employee;
+import com.sunsy.crud.bean.EmployeeExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EmployeeMapper {
+    long countByExample(EmployeeExample example);
+
+    int deleteByExample(EmployeeExample example);
+
+    int deleteByPrimaryKey(Integer empId);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    List<Employee> selectByExample(EmployeeExample example);
+
+    Employee selectByPrimaryKey(Integer empId);
+
+    int updateByExampleSelective(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByExample(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+}
